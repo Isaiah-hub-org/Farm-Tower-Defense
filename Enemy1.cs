eb8d0ef65f6f99cee34517230a859848f88572df
Mary Lynn <mlynn@ftstudent.org>
2026-02-17 12:06:17 -0500

    Enemy with code
    adding script to enemy and adding to map

---
+++ Enemy1.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+
+public partial class Enemy1 : CharacterBody2D
+{
+	[Export] public float Speed = 50f;
+
+	private PathFollow2D pathFollow;
+
+	public override void _Ready()
+	{
+		pathFollow = GetParent<PathFollow2D>();
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		// Move along the path
+		pathFollow.Progress += Speed * (float)delta;
+	}
+	public override void _Process(double delta)
+	{
+		var pathFollow = GetParent<PathFollow2D>();
+		pathFollow.Progress += Speed * (float)delta;
+
+		if (pathFollow.ProgressRatio >= 1.0f)
+		{
+		pathFollow.QueueFree(); // removes enemy + pathfollow
+		}
+	}
+
+}
