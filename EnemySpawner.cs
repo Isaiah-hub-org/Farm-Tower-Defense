aa17c75dd91512038766a5b295604111eec7aab2
Mary Lynn <mlynn@ftstudent.org>
2026-02-18 11:41:43 -0500

    assigning
    this is the reason why my enemy wasn't spawning

--- EnemySpawner.cs
+++ EnemySpawner.cs
@@ -3,43 +3,43 @@ using System;
 
 public partial class EnemySpawner : Node2D
 {
-	[Export] public PackedScene EnemyScene; // The enemy to spawn
-	[Export] public Path2D Path;             // The path enemies will follow
-	[Export] public float SpawnInterval = 2f;
+	[Export] public PackedScene EnemyScene;
+	[Export] public Path2D Path;
+	[Export] public float SpawnInterval = 3f;
 	[Export] public float EnemySpeed = 50f;
 
 	private Timer _spawnTimer;
 
 	public override void _Ready()
 	{
-		// Create a repeating timer
+		if (EnemyScene == null || Path == null)
+		{
+			GD.PrintErr("EnemyScene or Path is not assigned!");
+			return;
+		}
+
 		_spawnTimer = new Timer();
 		_spawnTimer.WaitTime = SpawnInterval;
 		_spawnTimer.OneShot = false;
-		AddChild(_spawnTimer);
+		_spawnTimer.Autostart = true;
 
-		// Call SpawnEnemy() every time the timer finishes
+		AddChild(_spawnTimer);
 		_spawnTimer.Timeout += SpawnEnemy;
-		_spawnTimer.Start();
 	}
 
 	private void SpawnEnemy()
 	{
-		
+		GD.Print("Spawning enemy...");
 
-		// 1) Create a PathFollow2D node
 		PathFollow2D pathFollow = new PathFollow2D();
 		pathFollow.Loop = false;
-		pathFollow.Progress = 0; // Start at beginning of path
+		pathFollow.Progress = 0;
 
-		// 2) Attach PathFollow2D to Path2D
 		Path.AddChild(pathFollow);
 
-		// 3) Create the enemy
 		Enemy enemy = EnemyScene.Instantiate<Enemy>();
 		enemy.Speed = EnemySpeed;
 
-		// 4) Put the enemy on the path
 		pathFollow.AddChild(enemy);
 	}
 }
