ba487138033bbcc752e8ac6e2991b17af7f37248
Isaiah Pierre <ipierre0@ftstudent.org>
2026-02-19 12:00:45 -0500

    almost done

---
+++ Scenes/MainMenu.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+
+
+public partial class Menu : Control
+{
+	public void OnPlayPressed() {
+		 //GetTree().ChangeSceneToFile("res://Scenes/Level.tscn");
+		
+	}
+	public void OnSettingsPressed() {
+		//GetTree().ChangeSceneToFile("res://Scenes/settings.tscn");
+	}
+	public void OnLoadPressed(){
+		
+	}
+    public void OnExitPressed(){
+		
+	}
+	
+}
