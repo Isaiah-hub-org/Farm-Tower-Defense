d26d6a5d94c1f84e4272baef5eb5cc7f66e67942
JonJonesCodesForFun <jpgorske08@gmail.com>
2026-02-23 12:12:30 -0500

    Edited

--- Arrow.cs
+++ Arrow.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class AnimatedSprite2d : AnimatedSprite2D
+public partial class Arrow : Node2D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
