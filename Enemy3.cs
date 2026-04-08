d8860e7fbf2edc61f57e7b7550f92bf7bef5b215
JonJonesCodesForFun <jpgorske08@gmail.com>
2026-04-08 12:21:48 -0400

    Continue

--- Enemy3.cs
+++ Enemy3.cs
@@ -8,32 +8,30 @@ public partial class Enemy3 : CharacterBody2D
 {
 	public int MaxHealth = 50; 
 	public int CurrentHealth; 
+	[Export] public float Speed = 95f;
+	private PathFollow2D pathFollow;
+	protected ProgressBar HealthBar;
 	
 	[Signal]
 	public delegate void HealthChangedEventHandler(int oldHealth, int newHealth);
 	
 	public virtual void _Ready() {
 		CurrentHealth = MaxHealth;
-		//enemyHealthBar = GetNode<ProgressBar>("enemyHealthBar");
-		//enemyHealthBar.MaxValue = MaxHealth;
-		//enemyHealthBar.Value = CurrentHealth;
+	
+		HealthBar = GetNode<ProgressBar>("enemyHealthBar");
+		HealthBar.MaxValue = MaxHealth;
+		HealthBar.Value = CurrentHealth;
 		
+		pathFollow = GetParent<PathFollow2D>();
 	}
 
 	public virtual void TakeDamage(int damage) {
 		EmitSignal(SignalName.HealthChanged, CurrentHealth, CurrentHealth - damage);
 		CurrentHealth -= damage;
+		HealthBar.Value -= damage;
 	}
-}
 
-	public partial class Enemy3 : CharacterBody2D [Export] public float Speed = 95f;
 
-	private PathFollow2D pathFollow;
-
-	public override void _Ready()
-	{
-		pathFollow = GetParent<PathFollow2D>();
-	}
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -45,5 +43,6 @@ public partial class Enemy3 : CharacterBody2D
 		{
 			pathFollow.QueueFree(); 
 			QueueFree();
+		}
 	}
 }
