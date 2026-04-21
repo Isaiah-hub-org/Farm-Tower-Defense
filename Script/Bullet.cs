fe00cc06cc877eb3f3d37b4788efd4f95958ea94
Isaiah Pierre <ipierre0@ftstudent.org>
2026-04-21 16:41:50 -0400

    hm

---
+++ Script/Bullet.cs
@@ -0,0 +1,26 @@
+using Godot;
+
+public partial class Bullet : Area2D
+{
+	[Export] public float Speed = 500f;
+	
+	private bool hasHit = false;
+	
+	public override void _Process(double delta)
+	{
+		 Position += Transform.X * Speed * (float)delta;
+	}
+
+	private void OnEnemyEntered(Node2D body)
+	{
+		
+		if (hasHit) return;
+		if (body.IsInGroup("enemies"))
+		{
+			GD.Print("hit");
+			hasHit = true;
+			
+			QueueFree();
+		}
+	}
+}
