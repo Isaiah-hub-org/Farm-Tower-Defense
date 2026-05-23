bb8a91966a60835207dd01cddb924d4ee3587379
Isaiah Pierre <ipierre0@ftstudent.org>
2026-05-22 22:52:59 -0400

    pause

---
+++ PauseMenu.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+public partial class GameManager : Node
+{
+    public override void _Process(double delta)
+    {
+        if (Input.IsActionJustPressed("ui_cancel"))
+        {
+            TogglePause();
+        }
+    }
+
+    private void TogglePause()
+    {
+        GetTree().Paused = !GetTree().Paused;
+    }
+}
